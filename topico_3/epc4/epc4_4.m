a6f2be6bd46363310395fc38c3c2cbbb1acc72ca
Guilherme <oliveira.gui5@gmail.com>
2020-09-30 20:11:33 +0200

    epc4

--- topico_3/epc4/epc4_4.m
+++ topico_3/epc4/epc4_4.m
@@ -3,13 +3,15 @@ function [N,Fs,X] = epc4_4(signal,sampleRate)
   x = fft(signal);
   Fs = sampleRate;
   
-  X=[];
-  for m = 1:N
-    mag = 2* abs(x(m)/N);
-    if mag > 0.95
-      f = m *(Fs/N);
-      ang = angle(x(m)/N);
-      X = [X, [mag;f;ang]];
+  %X[0] termo DC
+  X=[2* abs(x(1)/N);1 *(Fs/N);angle(x(1)/N)];
+  
+  for m = 2:N
+    mag = 2* abs(x(m)/N);   % calculo da magnitute
+    if mag > 0.6
+      f = m *(Fs/N);        % calculo da frequencia
+      ang = angle(x(m)/N);  % calculo da fase
+      X = [X, [mag;f;ang]]; % adiciona no vetor X
     end
   end
   
