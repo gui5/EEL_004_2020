4faaf4cb153c39bef96297d17fbce51fc98c3a6e
Guilherme de Oliveira <oliveira.gui5@gmail.com>
2020-12-14 10:42:16 -0300

    Update convolution.m
    added explanations

--- prova_3/convolution.m
+++ prova_3/convolution.m
@@ -1,17 +1,20 @@
 function Y = convolution(x,h)
 
-m=length(x);
-n=length(h);
-X=[x,zeros(1,n)];%zero padding 
-H=[h,zeros(1,m)]; %zero padding 
+m=length(h);
+n=length(x);
 
-for i=1:n+m-1
-  Y(i)=0;
-  for j=1:m
-    if(i-j+1>0)
-      Y(i)=Y(i)+X(j)*H(i-j+1);
-    endif
-  end
-end
+%zero padding, deixar ambos os vetores do mesmo tamanho
+X=[x,zeros(1,m)]
+H=[h,zeros(1,n)]
+
+    for i=1:n+m-1 % tamanho final do vetor de saida n+m-1 ou 2*length(X)-1
+        Y(i)=0; % inicia com zero o elemento i do vetor de saida.
+        for j=1:length(X)
+            if(i-j+1>0) % posiao nao pode ser menor ou igual a zero
+                Y(i)=Y(i)+X(j)*H(i-j+1); %computa a convolucao
+            else
+            end
+        end
+    end
 
 endfunction
