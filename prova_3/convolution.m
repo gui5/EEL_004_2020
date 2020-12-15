fc06f6f8d953fad171deb0c9c1ccba578cc79dfe
Guilherme de Oliveira <oliveira.gui5@gmail.com>
2020-12-14 21:28:07 -0300

    prova 3

--- prova_3/convolution.m
+++ prova_3/convolution.m
@@ -1,15 +1,15 @@
-function Y = convolution(x,h)
+function Y = convolution(kernel,data)
 
-m=length(h);
-n=length(x);
+m=length(kernel);
+n=length(data);
 
 %zero padding, deixar ambos os vetores do mesmo tamanho
-X=[x,zeros(1,m)]
-H=[h,zeros(1,n)]
+X=[kernel,zeros(1,m)]
+H=[data,zeros(1,n)]
 
     for i=1:n+m-1 % tamanho final do vetor de saida n+m-1 ou 2*length(X)-1
         Y(i)=0; % inicia com zero o elemento i do vetor de saida.
-        for j=1:length(X)
+        for j=1:length(H)
             if(i-j+1>0) % posiao nao pode ser menor ou igual a zero
                 Y(i)=Y(i)+X(j)*H(i-j+1); %computa a convolucao
             else
